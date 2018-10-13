35c155c86054724d77315f7082b5440d044c1128
Jacob Ferrero <jacobaferrero@gmail.com>
2018-10-13 00:15:42 -0500

    Initial commit

---
+++ app/src/main/java/com/queatz/permanentmemory/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.queatz.permanentmemory
+
+import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.design.widget.Snackbar
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        actionButton.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+        menuButton.setOnClickListener { drawerLayout.openDrawer(GravityCompat.START) }
+
+        navigationView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
